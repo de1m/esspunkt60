337c76bde52bb43427cbc759b1e7fc57cb6a8cac
de1m <wlkalexander@gmail.com>
2017-08-05 20:23:33 +0200

    add checking of upload folder

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var express         = require('express');
-var app             = express();
-var server          = require('http').createServer(app);
-var io              = require('socket.io')(server);
-var fs              = require('fs');
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var fs = require('fs');
 
 var appPort = process.env.APPPORT || "5000"
 
@@ -15,7 +15,7 @@ server.listen(appPort, function () {
 });
 
 //App components
-var routes      = require('./app/routes');
+var routes = require('./app/routes');
 
 app.use('/', routes);
 
@@ -23,3 +23,38 @@ io.attach(server);
 
 var socket = require('./app/socket');
 socket.start(io);
+
+checkDirectory(__dirname + '/public/upload', function (err, uploadDir) {
+  if (err) {
+    console.log(err);
+  } else {
+    checkDirectory(__dirname + '/public/upload/doc', function (err, docDir) {
+      if (err) {
+        console.log(err);
+      }
+    })
+    checkDirectory(__dirname + '/public/upload/images', function (err, docDir) {
+      if (err) {
+        console.log(err);
+      }
+    })
+  }
+});
+
+function checkDirectory(directory, callback) {
+  fs.exists(directory, function (err, stat) {
+    if (err) {
+      return callback(err, null);
+    } else {
+      if (!stat) {
+        fs.mkdir(directory, function (err, result) {
+          if (err) {
+            return callback(err, null);
+          } else {
+            return callback(null, result);
+          }
+        })
+      }
+    }
+  })
+}
