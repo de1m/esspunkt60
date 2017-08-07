aa178659eec076ed7e6ef3457d96a5e0338eeb33
a.walker <a.walker@adito.de>
2017-08-07 09:17:53 +0200

    change to mongo official image, disabled mongo auth, fix error in mongoose depricated

--- app/database/index.js
+++ app/database/index.js
@@ -1,21 +1,28 @@
 'use strict';
 
 var Mongoose = require('mongoose');
-
+var auth = process.env.AUTH || 'false';
 var mongoAddr = process.env.MONGOADDR || 'localhost';
 var mongoPort = process.env.MONGOPORT || '27017';
 var mongoDb = process.env.MONGODB || 'esspunkt60';
 var mongoUser = process.env.MONGOUSER || 'essen';
 var mongoPass = process.env.MONGOPASS || 'essen6003';
 
-var dbURI = 'mongodb://' +
+if(auth === true){
+    var dbURI = 'mongodb://' +
     mongoUser + ":" +
     mongoPass + "@" +
     mongoAddr + ":" +
     mongoPort + "/" +
     mongoDb;
+} else {
+    var dbURI = 'mongodb://' +
+    mongoAddr + ":" +
+    mongoPort + "/" +
+    mongoDb;
+}
 
-Mongoose.connect(dbURI);
+Mongoose.connect(dbURI, { useMongoClient: true });
 
 // Throw an error if the connection fails
 Mongoose.connection.on('error', function (err) {
