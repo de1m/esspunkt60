33c722742be41ffc816eb1f4a1b4db9789cae87b
a.walker <a.walker@adito.de>
2018-07-13 12:01:04 +0200

    add

--- front/src/components/NewEatPoint.vue
+++ front/src/components/NewEatPoint.vue
@@ -1,15 +1,22 @@
 <template>
   <div class="info">
     <header-nav></header-nav>
-    <div class="uk-container uk-container-center uk-container-expand">
-      <div class="uk-grid uk-align-center uk-text-center">
+    <!-- <div class="uk-container uk-container-expand">
+      <div class="uk-grid uk-align-center">
         <div class="uk-width-auto">
           <h3 class="uk-heading-line uk-text-center"><span v-bind:data="selectNewEatpointText"> {{ selectNewEatpointText }}</span></h3>
+          <div class="uk-container">
+            <div class="uk-grid uk-width-xxlarge">
+              <div class="uk-width-1-3" style="border: 1px solid red;">t</div>
+              <div class="uk-width-1-3" style="border: 1px solid red;">t</div>
+              <div class="uk-width-1-3" style="border: 1px solid red;">t</div>
+            </div>
+          </div>
           <h3 class="uk-heading-line uk-text-center"><span v-bind:data="createNewEatpointText"> {{ createNewEatpointText }}</span></h3>
           <vk-button v-on:click="goBack">Zurück</vk-button>
         </div>
       </div>
-    </div>
+    </div> -->
   </div>
 </template>
 
